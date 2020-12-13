935d21763eedfff06e201b7aff7006dbd039e3d8
huilee <huilee9999@gmail.com>
2020-12-12 21:05:21 -0500

    changed some strings

--- app/src/main/java/com/hannah/demo/ui/mine/MineFragment.kt
+++ app/src/main/java/com/hannah/demo/ui/mine/MineFragment.kt
@@ -4,28 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.hannah.demo.R
+import androidx.fragment.app.viewModels
+import com.hannah.demo.databinding.FragmentMineBinding
 
 class MineFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: MineViewModel
+    private val mineViewModel by viewModels<MineViewModel>()
+
+    private lateinit var fragmentMineBinding: FragmentMineBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(MineViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_mine, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        fragmentMineBinding = FragmentMineBinding.inflate(layoutInflater, container, false).apply {
+            viewmodel = mineViewModel
+        }
+        return fragmentMineBinding.root
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        mineViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+    }
+
+
 }
